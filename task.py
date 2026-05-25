fa581654056dba4e6e9c360c0e386c0c73cc795d
franc <f.falcone26@studenti.unisa.it>
2026-05-25 16:48:56 +0200

    Implementata la funzione: calcola_polinomio_taylor

--- task.py
+++ task.py
@@ -1,6 +1,6 @@
 import sympy
 from typing import Dict
-
+import math
 # Controlla il file readme.md per i dettagli su ciascun sub-task
 
 def calcola_derivata(espressione: str, variabile: str) -> sympy.Expr:
@@ -20,7 +20,8 @@ def calcola_limite(espressione: str, variabile: str, punto: str) -> sympy.Expr:
 
 def calcola_polinomio_taylor(espressione: str, variabile: str, punto: float, ordine: int) -> sympy.Expr:
     """Sub-task 4: Calcolare una Serie di Taylor."""
-    pass
+    approx = sympy.series(espressione, variabile, punto, ordine)
+    return approx
 
 def risolvi_sistema_lineare(eq1: str, eq2: str, var1: str, var2: str) -> Dict[sympy.Symbol, sympy.Expr]:
     """Sub-task 5: Risolvere un Sistema Lineare."""
