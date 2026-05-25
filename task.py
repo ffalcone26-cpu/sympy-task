8e05b792bf256e94e57eaa518f8da936378e1afc
franc <f.falcone26@studenti.unisa.it>
2026-05-25 15:55:57 +0200

    Implementata la funzione: calcola_derivata

--- task.py
+++ task.py
@@ -5,7 +5,8 @@ from typing import Dict
 
 def calcola_derivata(espressione: str, variabile: str) -> sympy.Expr:
     """Sub-task 1: Calcolare una Derivata."""
-    pass
+    df = sympy.diff(espressione, variabile)
+    return df
 
 def calcola_integrale_definito(espressione: str, variabile: str, estremo_inf: float, estremo_sup: float) -> sympy.Expr:
     """Sub-task 2: Calcolare un Integrale Definito."""
